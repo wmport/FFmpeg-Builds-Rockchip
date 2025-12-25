92d32e96e1767bf01d20d784fffa24d781f7be0a
wmport <159494449+wmport@users.noreply.github.com>
2025-12-25 08:16:42 +0200

    Update 8.0-rk.sh

--- addins/8.0-rk.sh
+++ addins/8.0-rk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-FFMPEG_REPO="https://github.com/nyanmisaka/ffmpeg-rockchip"
-GIT_BRANCH="8.0"
+FFMPEG_REPO="https://code.ffmpeg.org/Kwiboo/FFmpeg"
+GIT_BRANCH="v4l2request-2025-v3-rkvdec"
